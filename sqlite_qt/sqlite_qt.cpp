145efc6f6471d769106932de07b1251181332b45
cesspuller <m3dusssa@gmail.com>
2024-02-27 17:57:27 +0300

    фикс ошибок в бд

--- sqlite_qt/sqlite_qt.cpp
+++ sqlite_qt/sqlite_qt.cpp
@@ -10,31 +10,74 @@ TMainWindow::TMainWindow( QWidget* parent ) : QMainWindow( parent )
 
    query = new QSqlQuery( dataBase );
 
-   query->exec( "CREATE TABLE customers ("
+   query->exec( 
+                "CREATE TABLE IF NOT EXISTS customers ("
                 "id INTEGER PRIMARY KEY AUTOINCREMENT,"
                 "name TEXT,"
                 "gender TEXT,"
-                "age INTEGER );" );
+                "age INTEGER );" 
+              );
 
-   query->exec( "CREATE TABLE products ("
+   qDebug() << "ERROR:" << query->lastError().text();
+   
+   query->exec( 
+                "CREATE TABLE IF NOT EXISTS products ("
                 "id INTEGER PRIMARY KEY AUTOINCREMENT,"
                 "name TEXT,"
                 "gender TEXT,"
-                "FOREIGN KEY (id) REFERENCES customers(id) ON DELETE CASCADE )"  );
+                "FOREIGN KEY (id) REFERENCES customers(id) ON DELETE CASCADE )"  
+              );
 
-   query->exec( "CREATE TABLE orders ("
+   qDebug() << "ERROR:" << query->lastError().text();
+   
+   query->exec( 
+                "CREATE TABLE IF NOT EXISTS  orders ("
                 "id INTEGER PRIMARY KEY AUTOINCREMENT,"
                 "age INTEGER,"
-                "FOREIGN KEY (age) REFERENCES customers(age) ON DELETE CASCADE );" );
+                "FOREIGN KEY (age) REFERENCES customers(age) ON DELETE CASCADE );" 
+              );
 
-   //query->exec( "PRAGMA foreign_keys = ON" );
+   qDebug() << "ERROR:" << query->lastError().text();
 
-   query->exec( "CREATE TRIGGER insert_products "
-                "AFTER INSERT ON customers "
-                "BEGIN "
-                "INSERT INTO products (name, gender) "
-                "VALUES (NEW.name, NEW.gender); "
-                "END;" );
+   //query->exec( 
+   //             "CREATE TABLE departments ("
+   //             "department_id INTEGER PRIMARY KEY AUTOINCREMENT,"
+   //             "department_name VARCHAR );"
+   //           );
+   //
+   //qDebug() << "ERROR:" << query->lastError().text();
+   //
+   //query->exec( 
+   //             "CREATE TABLE employees ("
+   //             "employee_id INTEGER PRIMARY KEY AUTOINCREMENT,"
+   //             "last_name VARCHAR NOT NULL,"
+   //             "first_name VARCHAR,"
+   //             "department_id INTEGER,"
+   //             "CONSTRAINT fk_departments"
+   //             "FOREIGN KEY(department_id)"
+   //             "REFERENCES departments( department_id )"
+   //             "ON DELETE CASCADE );"
+   //           );
+
+   //query->exec( "CREATE TRIGGER insert_products "
+   //             "AFTER INSERT ON departments "
+   //             "BEGIN "
+   //             "INSERT INTO employees (department_id ) "
+   //             "VALUES (NEW.department_id ); "
+   //             "END;" );
+
+   query->exec( "PRAGMA foreign_keys = ON" );
+
+   qDebug() << "ERROR:" << query->lastError().text();
+
+   //query->exec( "CREATE TRIGGER IF NOT EXISTS insert_products "
+   //             "AFTER INSERT ON customers "
+   //             "BEGIN "
+   //             "INSERT INTO products (name, gender) "
+   //             "VALUES (NEW.name, NEW.gender); "
+   //             "END;" );
+
+   qDebug() << "ERROR:" << query->lastError().text();
 
    contactsModel = new QSqlRelationalTableModel( this, dataBase );
    contactsModel->setTable( "customers" );
@@ -55,7 +98,7 @@ TMainWindow::TMainWindow( QWidget* parent ) : QMainWindow( parent )
 
    ui.tableView->setModel( contactsModel );
    ui.tableView_2->setModel( phoneNumbersModel );
-   ui.tableView_3->setModel( emailsModel );
+   //ui.tableView_3->setModel( emailsModel );
 
    connect( ui.pushButton_2, SIGNAL( clicked() ), this, 
                              SLOT( qAddEntry( ) ) );
@@ -70,8 +113,14 @@ TMainWindow::TMainWindow( QWidget* parent ) : QMainWindow( parent )
 };
 
 void TMainWindow::qAddEntry()
-{
+{   
    contactsModel->insertRow( contactsModel->rowCount() );
+
+   bool a = contactsModel->submitAll();
+
+   qDebug() << "ERROR:" << contactsModel->lastError().text();
+   qDebug() << "ERROR:" << query->lastError().text();
+   qDebug() << "ERROR:" << dataBase.lastError().text();
    
    //if ( !contactsModel->submitAll() ) 
    //{
@@ -88,6 +137,16 @@ void TMainWindow::qDeleteEntry()
    for ( auto& elem : selectedRows )
    {
       contactsModel->removeRow( elem );
+
+
+      qDebug() << "ERROR:" << contactsModel->lastError().text();
+      qDebug() << "ERROR:" << query->lastError().text();
+      qDebug() << "ERROR:" << dataBase.lastError().text();
+      //query->exec( "DELETE FROM customers WHERE id = 3" );
+      //
+      //qDebug() << "ERROR:" << query->lastError().text();
+      //
+      //auto f = elem.toString();
    }
 };
 
@@ -99,6 +158,7 @@ void TMainWindow::qSelectedRow( const QItemSelection&, const QItemSelection& )
 
    for ( auto& elem : selection )
    {
-      selectedRows.push_back( int( elem.row() ) );
+      //auto f = elem.data();
+      selectedRows.push_back( elem.row() );
    }
 };
