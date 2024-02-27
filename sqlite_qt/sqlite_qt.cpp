c5cc310ff9e4485f1f2c5d0987a1b179a1b0d8ac
cesspuller <m3dusssa@gmail.com>
2024-02-27 19:35:59 +0300

    реализовал каскадное удаление из таблиц

--- sqlite_qt/sqlite_qt.cpp
+++ sqlite_qt/sqlite_qt.cpp
@@ -13,92 +13,43 @@ TMainWindow::TMainWindow( QWidget* parent ) : QMainWindow( parent )
    query->exec( 
                 "CREATE TABLE IF NOT EXISTS customers ("
                 "id INTEGER PRIMARY KEY AUTOINCREMENT,"
-                "name TEXT,"
-                "gender TEXT,"
-                "age INTEGER );" 
+                "name TEXT NOT NULL,"
+                "gender TEXT NOT NULL,"
+                "age INTEGER NOT NULL );" 
               );
 
-   qDebug() << "ERROR:" << query->lastError().text();
-   
    query->exec( 
                 "CREATE TABLE IF NOT EXISTS products ("
                 "id INTEGER PRIMARY KEY AUTOINCREMENT,"
-                "name TEXT,"
-                "gender TEXT,"
+                "name TEXT NOT NULL,"
+                "gender TEXT NOT NULL,"
                 "FOREIGN KEY (id) REFERENCES customers(id) ON DELETE CASCADE )"  
               );
-
-   qDebug() << "ERROR:" << query->lastError().text();
    
    query->exec( 
                 "CREATE TABLE IF NOT EXISTS  orders ("
                 "id INTEGER PRIMARY KEY AUTOINCREMENT,"
-                "age INTEGER,"
-                "FOREIGN KEY (age) REFERENCES customers(age) ON DELETE CASCADE );" 
+                "age INTEGER NOT NULL,"
+                "FOREIGN KEY (id) REFERENCES customers(id) ON DELETE CASCADE );" 
               );
-
-   qDebug() << "ERROR:" << query->lastError().text();
-
-   //query->exec( 
-   //             "CREATE TABLE departments ("
-   //             "department_id INTEGER PRIMARY KEY AUTOINCREMENT,"
-   //             "department_name VARCHAR );"
-   //           );
-   //
-   //qDebug() << "ERROR:" << query->lastError().text();
-   //
-   //query->exec( 
-   //             "CREATE TABLE employees ("
-   //             "employee_id INTEGER PRIMARY KEY AUTOINCREMENT,"
-   //             "last_name VARCHAR NOT NULL,"
-   //             "first_name VARCHAR,"
-   //             "department_id INTEGER,"
-   //             "CONSTRAINT fk_departments"
-   //             "FOREIGN KEY(department_id)"
-   //             "REFERENCES departments( department_id )"
-   //             "ON DELETE CASCADE );"
-   //           );
-
-   //query->exec( "CREATE TRIGGER insert_products "
-   //             "AFTER INSERT ON departments "
-   //             "BEGIN "
-   //             "INSERT INTO employees (department_id ) "
-   //             "VALUES (NEW.department_id ); "
-   //             "END;" );
 
    query->exec( "PRAGMA foreign_keys = ON" );
 
-   qDebug() << "ERROR:" << query->lastError().text();
-
-   //query->exec( "CREATE TRIGGER IF NOT EXISTS insert_products "
-   //             "AFTER INSERT ON customers "
-   //             "BEGIN "
-   //             "INSERT INTO products (name, gender) "
-   //             "VALUES (NEW.name, NEW.gender); "
-   //             "END;" );
-
-   qDebug() << "ERROR:" << query->lastError().text();
-
    contactsModel = new QSqlRelationalTableModel( this, dataBase );
    contactsModel->setTable( "customers" );
-   //contactsModel->setEditStrategy( QSqlTableModel::OnFieldChange );
    contactsModel->select();
 
    phoneNumbersModel = new QSqlRelationalTableModel( this, dataBase );
    phoneNumbersModel->setTable( "products" );
-   //phoneNumbersModel->setEditStrategy( QSqlTableModel::OnFieldChange );
    phoneNumbersModel->select();
 
    emailsModel = new QSqlRelationalTableModel( this, dataBase );
    emailsModel->setTable( "orders" );
-   //emailsModel->setEditStrategy( QSqlTableModel::OnFieldChange );
-   //emailsModel->setRelation( 1, QSqlRelation( "customers", "id", "name" ) );
-   //emailsModel->setRelation( 2, QSqlRelation( "products", "id", "name" ) );
    emailsModel->select();
 
    ui.tableView->setModel( contactsModel );
    ui.tableView_2->setModel( phoneNumbersModel );
-   //ui.tableView_3->setModel( emailsModel );
+   ui.tableView_3->setModel( emailsModel );
 
    connect( ui.pushButton_2, SIGNAL( clicked() ), this, 
                              SLOT( qAddEntry( ) ) );
@@ -108,28 +59,13 @@ TMainWindow::TMainWindow( QWidget* parent ) : QMainWindow( parent )
    
    connect( ui.tableView->selectionModel(), SIGNAL( selectionChanged( const QItemSelection&, const QItemSelection& ) ), this,
                                             SLOT( qSelectedRow( const QItemSelection&, const QItemSelection& ) ) );
-
-   qDebug() << "Error inserting into customers table:" << dataBase.lastError().text();
 };
 
 void TMainWindow::qAddEntry()
-{   
+{
    contactsModel->insertRow( contactsModel->rowCount() );
-
-   bool a = contactsModel->submitAll();
-
-   qDebug() << "ERROR:" << contactsModel->lastError().text();
-   qDebug() << "ERROR:" << query->lastError().text();
-   qDebug() << "ERROR:" << dataBase.lastError().text();
-   
-   //if ( !contactsModel->submitAll() ) 
-   //{
-   //   qDebug() << "Error inserting into customers table:" << contactsModel->lastError().text();
-   //}
-   //else 
-   //{
-   //   qDebug() << "Customer added successfully";
-   //}
+   phoneNumbersModel->insertRow( phoneNumbersModel->rowCount() );
+   emailsModel->insertRow( emailsModel->rowCount() );
 };
 
 void TMainWindow::qDeleteEntry()
@@ -138,15 +74,9 @@ void TMainWindow::qDeleteEntry()
    {
       contactsModel->removeRow( elem );
 
-
-      qDebug() << "ERROR:" << contactsModel->lastError().text();
-      qDebug() << "ERROR:" << query->lastError().text();
-      qDebug() << "ERROR:" << dataBase.lastError().text();
-      //query->exec( "DELETE FROM customers WHERE id = 3" );
-      //
-      //qDebug() << "ERROR:" << query->lastError().text();
-      //
-      //auto f = elem.toString();
+      ui.tableView_2->repaint();
+      ui.tableView_3->repaint();
+      //qDebug() << "ERROR:" << phoneNumbersModel->lastError().text();
    }
 };
 
@@ -158,7 +88,7 @@ void TMainWindow::qSelectedRow( const QItemSelection&, const QItemSelection& )
 
    for ( auto& elem : selection )
    {
-      //auto f = elem.data();
       selectedRows.push_back( elem.row() );
    }
 };
+
